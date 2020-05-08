3c605d459f29fc3f91cd59d03673500cfafaba4b
srivatsav k <srivatsavkamarajugadda@gmail.com>
2020-05-07 23:52:50 -0400

    Corrected the code for errors

--- main.cpp
+++ main.cpp
@@ -1,53 +1,109 @@
 #include <iostream>
-#include <vector.h>
+#include <vector>
+#include <algorithm>
 
 /* This program outputs minimum number of steps required for a robot to reach goal position from start position
  * SimpleSearch method is implemented by opening the nodes with minimum g value (path with minimum number of open nodes) */
 
-using namespace std;
+std::vector <int> simpleSearch(std::vector < std::vector<int> > &arr,std::vector <int> &start,std::vector <int> &goal,std::vector < std::vector<int> > &delta){
+	/* This function returns the number of steps and goal position
+	 * Inputs:
+	 * 	arr -> grid for which search algorithm needs to be run
+	 * 	start -> start position for the agent
+	 * 	goal -> goal position for the agent
+	 * 	delta -> the actions that agent can take
+	 * 
+	 * Outputs:
+	 * 	result -> a vector containing the number of steps and goal position
+	 * */
 
-int simpleSearch(int &arr,int start,int goal){
-	i = 0; // initializing x index of the node
-	j = 0; // initializing y index of the node
-	g = 0; // g value at the current node
-	vector <int> closedNodes [arr.size()/arr[0].size()][arr[0].size()]{0} // initializing this as boolean 0 - open; 1- closed
-	vector <int> openNodes [1][3]{g,start}; // adding g value to the indexes
-	vector <int> node{start};
-	while(node != goal){
-		closedNodes[i][j] = 1;
-		openNodes.pop_back();
-		
-		// opening nodes
-		g++;
-		if (arr[i+1][j] != 1 && closedNodes[i+1][j] != 1 && i+1 < arr.size()/arr[0].size(){
-			openNodes.push_back({g,i+1,j})
-		}
-		if (arr[i-1][j] != 1 && closedNodes[i-1][j] != 1 && i-1 > 0){
-			openNodes.push_back({g,i-1,j})
-		}
-		if (arr[i][j+1] != 1 && closedNodes[i][j+1] != 1 && j+1 < arr.size()-1{
-			openNodes.push_back({g,i,j+1})
-		}
-		if (arr[i][j-1] != 1 && closedNodes[i][j-1] != 1 && j-1 > 0){
-			openNodes.push_back({g,i,j-1})
-		}
-		
-	}
+	int x{0}; // initializing x index of the node
+	int y{0}; // initializing y index of the node
+	int g{0}; // g value at the current node
 	
+	int x2{0}; // initializing x index for expanded nodes
+	int y2{0}; // initializing y index for expanded nodes
+	int g2{0}; // initializing g value for expanded nodes
+	
+	// initializing a vector with open nodes
+	std::vector <std::vector <int> > openNode{{g,start[0],start[1]}} ;
+	
+	// making a copy of the array for storing closedNodes
+	std::vector <std::vector<int> > closedNodes (arr);
+	
+	bool resign{false}; // if unable to find the path
+	bool found{false}; // if found the path
+	
+	
+	// expanding the nodes and finding the path
+	while(found == false && resign == false){
+		
+		// popping the node with lowest g value
+		std::sort(openNode.begin(),openNode.end());
+		std::reverse(openNode.begin(),openNode.end());
+		g = openNode[openNode.size()-1][0]; // g value of the node to be popped
+		x = openNode[openNode.size()-1][1]; // x index of the node to be popped
+		y = openNode[openNode.size()-1][2]; // y index of the node to be popped
+		closedNodes[x][y] = 1; // adding the node to the closedNodes, bool "1" => closed nodes
+		openNode.pop_back(); 
+		
+		// expanding nodes
+		size_t len_actions{delta.size()};
+		g2 = g + 1;
+		for (size_t i{0}; i < len_actions ; i++){
+			x2 = x + delta[i][0];
+			y2 = y + delta[i][1];
+			
+			// Checking if found the goal else checking if its a valid node to be opened
+			if (x2 == goal[0] && y2 == goal[1]){
+				found = true;
+			return {g2,x2,y2};
+			} else if (x2 >= 0 && x2 < arr.size() && y2 >= 0 && y2 < arr[0].size() && closedNodes[x2][y2] != 1){
+				openNode.push_back({g2,x2,y2});
+				closedNodes[x2][y2] = 1; 
+			}
+		}
+		
+		// checking for the resign condition => no more nodes to open
+		if (openNode.size() == 0){
+			resign = true;
+			return {-1,-1,-1};
+		}
+	}
 }
-
 
 int main()
 {
-	// Defining the discrete grid for the search plan
-	int arr [5][6] {{0,0,1,0,0,0},
-					{0,0,1,0,0,0},
-					{0,0,0,0,1,0},
-					{0,0,1,1,1,0},
-					{0,0,0,0,1,0}};
+	/* The function that runs the main script */
 	
-	int start [1][2] {{0,0}};
-	int goal [1][2] {{4,5}};
-					
+	// Defining the discrete grid for the search plan
+	std::vector< std::vector<int> > arr {{0,0,1,0,0,0},
+										{0,0,1,0,0,0},
+										{0,0,0,0,1,0},
+										{0,0,1,1,1,0},
+										{0,0,0,0,1,0}};
+
+	// Initializing actions for the agent
+	std::vector <std::vector <int> > delta {{-1,0}, // up
+											{1,0}, // down
+											{0,1}, // right
+											{0,-1}};// left
+
+	std::vector<int> start {0,0}; // start position
+	std::vector<int> goal {4,5}; // goal position
+	
+	std::vector <int> result(3); // initializing vector to store results
+	
+	result = simpleSearch(arr,start,goal,delta);
+	
+	// printing if the path was found or not
+	if (result[0] == -1){
+		std::cout << "no path found" << std::endl;
+	} else {
+		std::cout << "Path found" << std::endl;
+		std::cout << "Minimum number of steps: " << result[0] << std::endl;
+		std::cout << "Goal Co-ordinates reached: [ " << result[1] << " , " << result[2] << " ]" << std::endl; 
+	}
+	
 	return 0;
 }
